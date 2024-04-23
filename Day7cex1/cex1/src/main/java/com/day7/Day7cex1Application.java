f29f5cd04dd2d999ee6515d083b2f5dbf198d16e
Kiruthika-01 <124856724+Kiruthika-01@users.noreply.github.com>
2024-04-23 09:27:20 +0530

    Add files via upload

---
+++ Day7cex1/cex1/src/main/java/com/day7/Day7cex1Application.java
@@ -0,0 +1,5 @@
+package com.day7;
+
+public class Day7cex1Application {
+
+}
---
+++ Day7cex1/cex1/src/main/java/com/day7/SpringApplication.java
@@ -0,0 +1,10 @@
+package com.day7;
+
+public class SpringApplication {
+
+    public static void run(Class<Day7cex1Application> class1, String[] args) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+}
