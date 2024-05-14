38907a8885acb0e31f6c004b569c921c618afe8b
Kiruthika-01 <124856724+Kiruthika-01@users.noreply.github.com>
2024-05-14 21:56:07 +0530

    Add files via upload

---
+++ stday10cex2/cex2/src/main/java/com/BraveDriver.java
@@ -0,0 +1,13 @@
+package com;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BraveDriver extends ChromeDriver {
+    
+    static ChromeOptions options =  new ChromeOptions().setBinary("C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe");
+    
+    public BraveDriver()    {
+        super(ChromeDriverService.createDefaultService(),options);
+    }
+}
